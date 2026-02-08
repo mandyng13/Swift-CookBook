0d1067f14dc83b3888ddef087dd727f41625abe8
mandy <mandyng_13@icloud.com>
2026-02-08 13:56:57 +0100

    Initial Commit

---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Mandy Nguyen on 08.02.26.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
